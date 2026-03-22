18e3b3f67e8640a4e9b5dfc9fb6f982e6ae76d41
Fenrir784 <111286028+Fenrir784@users.noreply.github.com>
2026-03-22 10:58:38 +0300

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = 'e621tagger-v1';
+const CACHE_VERSION = 'v2';
+const CACHE_NAME = `e621tagger-${CACHE_VERSION}`;
 const urlsToCache = [
   '/',
   '/static/css/style.css',
   '/static/js/script.js',
-  '/static/manifest.json',
-  '/static/android-chrome-192x192.png',
-  '/static/android-chrome-512x512.png',
+  '/static/apple-touch-icon.png',
+  '/static/favicon-32x32.png',
+  '/static/favicon-16x16.png',
   '/static/favicon.ico',
-  '/static/egg_top.png',
-  '/static/egg_bottom.png',
+  '/static/manifest.json',
   '/static/f1.png',
   '/static/f2.png',
   '/static/f3.png',
@@ -17,17 +17,44 @@ const urlsToCache = [
   '/static/f6.png',
   '/static/f7.png',
   '/static/f8.png',
-  '/static/f9.png'
+  '/static/f9.png',
+  '/static/egg_top.png',
+  '/static/egg_bottom.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => {
+      return clients.claim();
+    }).then(() => {
+      clients.matchAll({ type: 'window' }).then(clients => {
+        clients.forEach(client => {
+          client.postMessage({ action: 'reload' });
+        });
+      });
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
